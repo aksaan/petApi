d77cded06925336a1628ecf04c88b160ffb5a4de
aks_an <aks_an07@mail.ru>
2023-08-28 22:03:26 +0700

    Проверка авторизации

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    console.log('Попытка авторизации. Заголовок: ', authHeader);
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token){
+        return res.status(401).json({ error : 'unauthorized' });
+    }
+
+    const secret = process.env.SECRET_KEY;
+    jwt.verify(token, secret, (err, user) => {
+        if(err){
+            return res.status(403).json({ error : 'forbidden' });
+        }
+
+        req.user = user;
+        next();
+    })
+}
+
+module.exports = {
+    auth
+}
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+const { auth } = require('../middlewares/auth');
 const router = express.Router();
 
 const controller = require("../controllers/userController");
@@ -7,6 +9,6 @@ router.post("/signup", controller.signup);
 router.post("/signin", controller.signin);
 router.put("/profile", controller.profile);
 router.put("/users/:id/delete", controller.deleteUser);
-router.get("/", controller.all)
+router.get("/", auth, controller.all)
 
 exports.router = router;
