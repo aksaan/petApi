64f626b3beadecf124f6be8205f9b46253502c0c
aks_an <aks_an07@mail.ru>
2023-09-03 17:10:35 +0700

    Добавление Приюта

--- controllers/shelterController.js
+++ controllers/shelterController.js
@@ -1,3 +1,24 @@
-exports.shelter = function(req, res){
-    console.log("/");
+const bcrypt = require('bcrypt');
+
+const Shelter = require('../models/shelter');
+const User = require('../models/user');
+
+const add = async (req, res) => {
+    const { ogrn, title, email, phone } = req.body;
+    if (!ogrn || !title || !email) {
+        return res.status(404).json({ error : 'missing required fields'});
+    }
+    const owner = req.user.id
+    const sameOwner = await Shelter.findOne({ owner });
+    if (sameOwner){
+        return res.status(409).json({ error : 'shelter with same owner already registered'});
+    }
+
+    const newOwner= new Shelter({ email, ogrn, title, phone});
+    await newOwner.save();
+
+    return res.status(201).json({ message : 'shelter successfully created' });
+}
+module.exports = {
+    add
 }
---
+++ models/shelter.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const shelterSchema = new Schema ({
+    title : {
+        type : String, 
+        minLength : 5,
+        maxLength : 50,
+    },
+    ogrn : {
+        type : Number,
+        length : 15
+    },
+    phone : {
+        type : Number,
+        length : 11,
+    },
+    email : {
+        type : String,
+        minLength : 5,
+        maxLength : 100,
+    },
+    address : {
+        type : String,
+        minLength : 8,
+        maxLength : 100,
+    },
+    owner : {
+        type : Schema.Types.ObjectId,
+        ref : "User",
+        required : true,
+    }, 
+    desription : {
+        type : String,
+        maxLength : 300,
+    }
+})
+
+const Shelter = mongoose.model("Shelter", shelterSchema);
+
+module.exports = Shelter;
