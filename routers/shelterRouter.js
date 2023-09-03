30a86563671a00e5246de776736979f1e92a3c90
aks_an <aks_an07@mail.ru>
2023-09-03 17:20:30 +0700

    изменения

--- routers/shelterRouter.js
+++ routers/shelterRouter.js
@@ -4,6 +4,5 @@ const controller = require("../controllers/shelterController");
 const router = express.Router();
 
 router.post("/add", controller.add);
-router.put("/", controller.sheltpage);
 
 exports.router = router;
