66e229ec1c78da3cc1a6392499a6c5290ace67bb
aks_an <aks_an07@mail.ru>
2023-09-05 20:19:40 +0700

    изменения в router

--- routers/shelterRouter.js
+++ routers/shelterRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const controller = require("../controllers/shelterController");
 
+const { auth } = require('../middlewares/auth');
+
 const router = express.Router();
 
 router.post("/add", controller.add);
