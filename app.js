f8683bae34adb33ac152562081d6945177ffe3f1
aks_an <aks_an07@mail.ru>
2023-09-18 20:39:36 +0700

    app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+app.use(bodyParser.json())
 app.use((req, res, next) => {
     console.log(req.body);
     console.log(req.headers);
