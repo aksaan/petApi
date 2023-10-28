451c7867d855ad07e07745a3a0b7edae3bd5fcd0
aks_an <aks_an07@mail.ru>
2023-10-28 21:40:53 +0700

    изменения в upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -4,7 +4,7 @@ const storage = multer.diskStorage({
     destination : (req, file, cb) => cb(null, "uploads/"),
     filename : (req, file, cb) => {
         const suffix = Date.now() + "-" + Math.round(Math.random() * 1E6);
-        cb(null, file.filename + "-" + suffix + "." + file.originalname.split(".").pop())
+        cb(null, file.fieldname + "-" + suffix + "." + file.originalname.split(".").pop())
     }
 })
 
