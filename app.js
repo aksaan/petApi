99f42c5ccce50da0e0e3a5e3a6346a922d30e60c
aks_an <aks_an07@mail.ru>
2023-11-11 19:00:27 +0700

    загрузка в папку avatars

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 
-app.use("/static", express.static(__dirname + "/uploads"))
+app.use("/uploads", express.static(__dirname + "/uploads"))
 app.use((req, res, next) => {
     console.log(req.body);
     console.log(req.headers);
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,7 +1,7 @@
 const multer  = require("multer");
 
 const storage = multer.diskStorage({
-    destination : (req, file, cb) => cb(null, "uploads/"),
+    destination : (req, file, cb) => cb(null, "uploads/avatars"),
     filename : (req, file, cb) => {
         const suffix = Date.now() + "-" + Math.round(Math.random() * 1E6);
         cb(null, file.fieldname + "-" + suffix + "." + file.originalname.split(".").pop())
